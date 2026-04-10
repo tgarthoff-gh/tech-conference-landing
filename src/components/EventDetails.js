0caeaf3c9b401d08407918baf2cad48a8f34695f
tgarthoff-gh <tgarthoff@gmail.com>
2026-04-10 15:49:35 +0200

    Add EventDetails component

---
+++ src/components/EventDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const EventDetails = () => {
+    return (
+        <div className="event-details">
+            <h2>Event Details</h2>
+            <p>Date: November 15, 2023</p>
+            <p>Location: Tech Center</p>
+        </div>
+    );
+};
+
+export default EventDetails;
