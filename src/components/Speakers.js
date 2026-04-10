49b3faa4555723295e1ecbb903a5207751ba89a0
tgarthoff-gh <tgarthoff@gmail.com>
2026-04-10 15:49:35 +0200

    Add Speakers component

---
+++ src/components/Speakers.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Speakers = () => {
+    return (
+        <div className="speakers">
+            <h2>Our Speakers</h2>
+            <ul>
+                <li>Jane Doe - AI Expert</li>
+                <li>John Smith - Blockchain Innovator</li>
+                <li>Mary Johnson - Cybersecurity Specialist</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Speakers;
