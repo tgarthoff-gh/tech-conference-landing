18fad6270db8b7bb30cc6f5c7259c9acc09e6ca8
tgarthoff-gh <tgarthoff@gmail.com>
2026-04-10 15:49:34 +0200

    Add Registration component

---
+++ src/components/Registration.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Registration = () => {
+    return (
+        <div className="registration">
+            <h2>Register Now</h2>
+            <button>Register</button>
+        </div>
+    );
+};
+
+export default Registration;
